7e8f7ef8c963f1548fdb1e8b7f67aac9c206fbce
msubrama <msubrama@INW1R90JSCYX.informatica.com>
2018-04-09 20:13:32 +0530

    Bubble Sort

--- InterviewPrep/src/com/learnings/sorting/BubbleSort.java
+++ InterviewPrep/src/com/learnings/sorting/BubbleSort.java
@@ -4,39 +4,33 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.Stream;
 /**
- * 
- * Step 1 − Set MIN to location 0
- * Step 2 − Search the minimum element in the list
- * Step 3 − Swap with value at location MIN
- * Step 4 − Increment MIN to point to next element
- * Step 5 − Repeat until list is sorted
+ * Bubble sort is a simple sorting algorithm. This sorting algorithm is comparison-based 
+ * algorithm in which each pair of adjacent elements is compared and the elements are swapped 
+ * if they are not in order. This algorithm is not suitable for large data sets as its average 
+ * and worst case complexity are of O(n2) where n is the number of items.
  * @author msubrama
  *
  */
-public class SelectionSort {
+public class BubbleSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		Scanner s=new Scanner(System.in);
 		int[] numbersToSort = Stream.of(s.nextLine().split(",")).mapToInt(i -> Integer.parseInt(i)).toArray();
 		s.close();
 		
 		int n = numbersToSort.length;
-		int minIndex = 0;
 		for(int i = 0; i < n-1; i++) {
-			minIndex = i;
 			for(int j = i; j < n; j++) {
-				if(numbersToSort[j] < numbersToSort[minIndex]) {
-					minIndex = j;
+				if(numbersToSort[i] > numbersToSort[j]) {
+					swap(numbersToSort,i,j);
 				}
 			}
-			swap(numbersToSort,i,minIndex);
 			System.out.println(Arrays.toString(numbersToSort));
 		}
 		System.out.println(Arrays.toString(numbersToSort));
 	}
-	
+
 	public static void swap(int[] arrNumber, int fromIndex, int toIndex) {
 		int temp = arrNumber[fromIndex];
 		arrNumber[fromIndex] = arrNumber[toIndex];
